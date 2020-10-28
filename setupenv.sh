544fb60364a52723c0e587830809b2f68fbf1a11
zeroway <M_O_Bz@163.com>
2020-10-28 16:10:14 +0800

    SSH command update

--- setupenv.sh
+++ setupenv.sh
@@ -1,21 +1,21 @@
-# cpu is ip address of server aa.bb.cc.dd
+#@cpu is ip address of server aa.bb.cc.dd
 # authorized root login
-cat ~/.ssh/id_rsa.pub | ssh -l root cpu "cat > ~/.ssh/authorized_keys"
+cat ~/.ssh/id_rsa.pub | ssh root@cpu "cat > ~/.ssh/authorized_keys"
 
 # add anonymous as root
-ssh -l root cpu "useradd -m -u 0 -o -g root -G root anonymous"
-ssh -l root cpu "echo 0 | passwd --stdin anonymous"
+ssh root@cpu "useradd -m -u 0 -o -g root -G root anonymous"
+ssh root@cpu "echo 0 | passwd --stdin anonymous"
 
 # config anonymous ssh login
-ssh -l root cpu "sed -i s'/root/root anonymous/' /etc/ssh/sshd_config"
-ssh -l root cpu "service sshd restart"
+ssh root@cpu "sed -i s'/root/root anonymous/' /etc/ssh/sshd_config"
+ssh root@cpu "service sshd restart"
 
-ssh -l anonymous cpu "mkdir ~/.ssh"
-cat ~/.ssh/id_rsa.pub | ssh -l anonymous cpu "cat > ~/.ssh/authorized_keys"
+ssh anonymous@cpu "mkdir ~/.ssh"
+cat ~/.ssh/id_rsa.pub | ssh anonymous@cpu "cat > ~/.ssh/authorized_keys"
 
 # config bash
 scp dot_bashrc_mini anonymous@cpu:~/.bashrc
 
 # config vim
-scp vim.tar anonymous@cpu:~/
-ssh -l anonymous cpu "tar xvf vim.tar"
+cat vim.tar | ssh anonymous@cpu "tar xvf -"
+ssh anonymous@cpu "ln -s ~/.vim/vimrc ~/.vimrc"
